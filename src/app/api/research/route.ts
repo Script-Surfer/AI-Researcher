f4bbb3e85d1f56a652f64d76df4ea6cc297f9117
Shounak.cs <shounakkumbhakar650@gmail.com>
2026-01-03 12:19:18 +0530

    frontend added

--- src/app/api/research/route.ts
+++ src/app/api/research/route.ts
@@ -7,17 +7,25 @@ export async function POST(request: Request){
     if(!topic){
         return Response.json({
             error: "Topic is empty."
-        }, {status: 500})
+        }, {status: 400})
     }
 
-    const prompt = `You are a research asistant. Given topic is ${topic}. Provide: 1. A concise overview of the topic.  2. 5 key subtopics from that topic 3. Important keywords  4. 5 research questions.`
+    const prompt = `You are a research asistant. Given topic is ${topic}. Provide the response in Markdown format:
+    1. A concise overview of the topic.
+    2. 5 key subtopics (bullet points).
+    3. Important keywords.
+    4. 5 research questions.`
 
-    const result = await generateText({
-        model: google("gemini-2.5-pro"),
-        prompt
-    })
-
-    return Response.json({
-        output: result.text
-    })
+    try {
+        const result = await generateText({
+            model: google("gemini-2.5-pro"),
+            prompt
+        })
+    
+        return Response.json({
+            output: result.text
+        })
+    } catch (error) {
+        return Response.json({ error: "failed to generate research"},{status: 500})
+    }
 }
